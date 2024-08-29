e89df69ff3bf37f8c83ca5c65ea0048c434574b1
Anmol Grewal <anmolsgrewal@gmail.com>
2024-08-29 16:14:47 -0700

    Fixing highscore case sensitive issue

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,4 +1,4 @@
-function FinishedScreen({ points, maxPossiblePoints, Highscore, dispatch }) {
+function FinishedScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
@@ -15,7 +15,7 @@ function FinishedScreen({ points, maxPossiblePoints, Highscore, dispatch }) {
         <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
-      <p className="highscore">(Highscore: {Highscore} points)</p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "restart" })}
